e411ceb2f37957fd0752bb5fc2c2e30579497a12
Jaemin0826 <shiibadog@gmail.com>
2025-07-04 18:56:13 +0900

    커밋

--- src/Display.jsx
+++ src/Display.jsx
@@ -3,14 +3,16 @@ import styled from "styled-components";
 const Mobile = styled.div`
     width: 402px;
     height: 874px;
+    background-color: #fff;
 `
 
-function Display (){
+function Display(){
+    return(
     <>
         <Mobile>
-            
         </Mobile>
     </>
+    )
 }
 
 export default Display
